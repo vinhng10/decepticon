5857eae7ce1b18963d36613e2a2792830730f49c
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-03-16 23:08:24 +0200

    add: race dataset and lightning data module.

--- project/data/data.py
+++ project/data/data.py
@@ -3,8 +3,10 @@ import sys
 import re
 import glob
 import json
+from pathlib import Path
 from tqdm import tqdm
 from collections import *
+from argparse import ArgumentParser
 
 import numpy as np
 import pandas as pd
@@ -13,6 +15,11 @@ import matplotlib.pyplot as plt
 import nltk
 from nltk import sent_tokenize, word_tokenize
 nltk.download('punkt')
+
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning import LightningDataModule
+
+from transformers import AutoTokenizer
 
 
 def tokenize(st):
@@ -54,93 +61,255 @@ def prepare_data(race, tokenizer):
     return inputs, outputs
 
 
-def preprocess_common(text):
-    """ Preprocess text.
+class RaceDataProcessor:
+    """ Race Data Processor """
+    def process_common(self, text):
+        """ Preprocess text.
 
-    Parameters
-    ----------
-    text: str
-        Text to be processed.
+        Parameters
+        ----------
+        text: str
+            Text to be processed.
 
-    Returns
-    -------
-    text: str
-        Processed text.
-    """
-    # Replace "`" with "'":
-    text = text.replace("`", "'")
-    # Remove "s6t----":
-    text = text.replace("s6t----", "")
-    # Remove [...]:
-    text = "".join(re.split(r"\[+.*\]*", text))
-    return text
+        Returns
+        -------
+        text: str
+            Processed text.
+        """
+        # Replace "`" with "'":
+        text = text.replace("`", "'")
+        # Remove "s6t----":
+        text = text.replace("s6t----", "")
+        # Remove [...]:
+        text = "".join(re.split(r"\[+.*\]*", text))
+        return text
 
+    def process_question(self, question):
+        """ Preprocess question.
 
-def preprocess_question(question):
-    """ Preprocess question.
+        Parameters
+        ----------
+        question: str
+            Question to be processed.
 
-    Parameters
-    ----------
-    question: str
-        Question to be processed.
-
-    Returns
-    -------
-    question: str
-        Processed question.
-    """
-    # Common preprocess:
-    question = preprocess_common(question)
-    # Remove question number and "." at the start
-    question = "".join(re.split(r"\A\d*\. *", question))
-    # Remove redundant duplicate of "-----":
-    question = " ".join(re.split(r"--+", question))
-    # Remove redundant whitespace:
-    question = " ".join(question.split())
-    return question
-
-
-def preprocess_options(options):
-    """ Preprocess options.
-
-    Parameters
-    ----------
-    options: list of str
-        Options to be processed.
-
-    Returns
-    -------
-    options: list of str
-        Processed options.
-    """
-    for i in range(len(options)):
+        Returns
+        -------
+        question: str
+            Processed question.
+        """
+        # Common process:
+        question = process_common(question)
+        # Remove question number and "." at the start
+        question = "".join(re.split(r"\A\d*\. *", question))
+        # Remove redundant duplicate of "-----":
+        question = " ".join(re.split(r"--+", question))
         # Remove redundant whitespace:
-        options[i] = preprocess_common(options[i])
+        question = " ".join(question.split())
+        return question
+
+    def process_options(self, options):
+        """ Preprocess options.
+
+        Parameters
+        ----------
+        options: list of str
+            Options to be processed.
+
+        Returns
+        -------
+        options: list of str
+            Processed options.
+        """
+        for i in range(len(options)):
+            # Remove redundant whitespace:
+            options[i] = process_common(options[i])
+            # Remove redundant whitespace:
+            options[i] = " ".join(options[i].split())
+        return options
+
+    def process_article(self, article):
+        """ Preprocess article.
+
+        Parameters
+        ----------
+        article: str
+            Article to be processed.
+
+        Returns
+        -------
+        article: str
+            Processed article.
+        """
+        # Common process:
+        article = process_common(article)
+        # Replace redundant duplicate of "-----" with "--":
+        article = re.sub(r"--+", "--", article)
         # Remove redundant whitespace:
-        options[i] = " ".join(options[i].split())
-    return options
+        article = " ".join(article.split())
+        return article
+
+    def process_data(self, data_path, save_path):
+        """ Preprocess data.
+
+        The function will perform preprocessing on all files in the directory
+        given in the "data_path". The processed data will be saved to the
+        directory given in the "save_path". The directory structure of the
+        processed files will be the same as that of the raw data files.
+
+        Parameters
+        ----------
+        data_path: str
+            Path to raw data directory.
+        save_path: str
+            Path to directory to save processed data.
+
+        Returns
+        -------
+        None
+        """
+        # Prepare path for saving processed data:
+        save_path = Path(save_path)
+        # Glob data paths:
+        paths = Path(data_path).glob("*/*/*")
+        # Process data:
+        for path in tqdm(paths):
+            with open(path) as f:
+                data = json.load(f)
+            # Process article:
+            data["article"] = process_article(data["article"])
+            # Process questions and options:
+            for i in range(len(data["questions"])):
+                data["questions"][i] = process_question(data["questions"][i])
+                data["options"][i] = process_options(data["options"][i])
+
+            # Create new file to save processed data:
+            dataset = path.parent.parent.stem
+            level = path.parent.stem
+            name = path.name
+            parent_path = save_path / dataset / level
+            parent_path.mkdir(parents=True, exist_ok=True)
+            dump_path = parent_path / name
+            dump_path.touch(exist_ok=True)
+
+            # Dump processed data to save path:
+            with open(dump_path, "w") as f:
+                json.dump(data, f)
+
+        print("Preprocess data successfully!")
 
 
-def preprocess_article(article):
-    """ Preprocess article.
+class RaceDataset(Dataset):
+    """ Race Dataset """
+    def __init__(self, data_paths):
+        """"""
+        super().__init__()
+        self.data_paths = data_paths
+        self.dataset = []
+        for path in data_paths:
+            with open(path) as f:
+                data = json.load(f)
+            for i in range(len(data["answers"])):
+                self.dataset.append({
+                    "answer": data["answers"][i],
+                    "options": data["options"][i],
+                    "question": data["questions"][i],
+                    "article": data["article"],
+                })
 
-    Parameters
-    ----------
-    article: str
-        Article to be processed.
+    def __len__(self):
+        """"""
+        return len(self.dataset)
 
-    Returns
-    -------
-    article: str
-        Processed article.
-    """
-    # Common preprocess:
-    article = preprocess_common(article)
-    # Replace redundant duplicate of "-----" with "--":
-    article = re.sub(r"--+", "--", article)
-    # Remove redundant whitespace:
-    article = " ".join(article.split())
-    return article
+    def __getitem__(self, idx):
+        """"""
+        return self.dataset[idx]
 
+
+class RaceDataModule(LightningDataModule):
+    """ Race Data Module """
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        """"""
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--data_path", type=str,
+                            help="Path to data.")
+        parser.add_argument("--batch_size", type=int, default=256,
+                            help="Batch size.")
+        parser.add_argument("--num_workers", type=int, default=8,
+                            help="Number of workers for data loading.")
+        parser.add_argument("--pretrained_model", type=str, default="prajjwal1/bert-tiny",
+                            help="Pretrained model.")
+        return parser
+
+    def __init__(self, hparams):
+        """"""
+        super().__init__()
+        self.hparams = hparams
+        self.tokenizer = AutoTokenizer.from_pretrained(hparams.pretrained_model)
+
+    def collate_fn(self, batch):
+        """"""
+        questions = []
+        articles = []
+        options = []
+        for item in batch:
+            questions.append(item["question"])
+            articles.append(item["article"])
+            options.append(self.tokenizer.sep_token.join(item["options"]))
+        return {
+            "questions": self.tokenizer(questions, padding=True, truncation=True, return_tensors="pt"),
+            "articles": self.tokenizer(articles, padding=True, truncation=True, return_tensors="pt"),
+            "options": self.tokenizer(options, padding=True, truncation=True, return_tensors="pt"),
+        }
+
+    def prepare_data(self):
+        """"""
+        pass
+
+    def setup(self, stage=None):
+        """"""
+        train_paths = Path(self.hparams.data_path).glob("train/*/*")
+        val_paths = Path(self.hparams.data_path).glob("dev/*/*")
+        test_paths = Path(self.hparams.data_path).glob("test/*/*")
+        # Prepare datasets
+        self.trainset = RaceDataset(train_paths)
+        self.valset = RaceDataset(val_paths)
+        self.testset = RaceDataset(test_paths)
+
+    def train_dataloader(self):
+        """"""
+        self.train_loader = DataLoader(
+            self.trainset,
+            batch_size=self.hparams.batch_size,
+            shuffle=True,
+            num_workers=self.hparams.num_workers,
+            pin_memory=True,
+            collate_fn=self.collate_fn,
+        )
+        return self.train_loader
+
+    def val_dataloader(self):
+        """"""
+        self.val_loader = DataLoader(
+            self.valset,
+            batch_size=self.hparams.batch_size,
+            shuffle=False,
+            num_workers=self.hparams.num_workers,
+            pin_memory=True,
+            collate_fn=self.collate_fn,
+        )
+        return self.val_loader
+
+    def test_dataloader(self):
+        """"""
+        self.test_loader = DataLoader(
+            self.testset,
+            batch_size=self.hparams.batch_size,
+            shuffle=False,
+            num_workers=self.hparams.num_workers,
+            pin_memory=True,
+            collate_fn=self.collate_fn,
+        )
+        return self.test_loader
 
 
