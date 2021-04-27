b3aca0e3b1aa6ebc5186f0b2c74639ab124fdf50
Haonan Liu <haonan.liu.edu@gmail.com>
2021-04-27 10:07:11 +0300

    update main

--- project/main.py
+++ project/main.py
@@ -26,11 +26,11 @@ if __name__ == "__main__":
 
     # Choose the model
     # from models.transformer import RaceModule
-    # from models.rnn import RaceModule
-    from models.t5 import RaceModule
+    from models.rnn import RaceModule
+    # from models.t5 import RaceModule
 
     batch_fn = None
-    collate_fn = t5_collate_fn
+    collate_fn = None
 
     pl.seed_everything(1234)
 
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     parser = RaceDataModule.add_model_specific_args(parser)
     parser = RaceModule.add_model_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
-    config = yaml.load(open("project/configs/t5.yaml"), Loader=yaml.FullLoader)
+    config = yaml.load(open("configs/rnn.yaml"), Loader=yaml.FullLoader)
     args = parser.parse_args(serialize_config(config))
 
     fx_dm = RaceDataModule(args, collate_fn)
@@ -57,11 +57,10 @@ if __name__ == "__main__":
                                   mode="min")
 
     # Logger:
-    # logger = TensorBoardLogger('models/logs/')
-    logger = NeptuneLogger(project_name="carlomarxdk/T5-for-RACE",
-                           params=vars(args),
-                           experiment_name="T5 finetuning to race: %s" % str(args.version),
-                           api_key='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiMTY1YzBlY2QtOTFlMS00Yzg2LWJiYzItNjQ2NDlhOGRhN2M5In0=')
+    logger = TensorBoardLogger('models/logs/')
+    # logger = NeptuneLogger(project_name="haonan-liu/RACE",
+    #                        params=vars(args),
+    #                        api_key='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5OTI4YmE4My0wYmNmLTQwZWYtYTM4YS05ZTNiNjJkNDc2MTUifQ==')
 
     # Trainer:
     trainer = pl.Trainer.from_argparse_args(
@@ -70,12 +69,13 @@ if __name__ == "__main__":
         callbacks=earlystopping,
         logger=logger
     )
-    trainer.fit(fx_model, fx_dm)
-    trainer.test(fx_model, test_dataloaders=fx_dm.test_dataloader())
+    # trainer.fit(fx_model, fx_dm)
+    # trainer.test(fx_model, test_dataloaders=fx_dm.test_dataloader())
 
-    fx_infer = RaceModule.load_from_checkpoint(checkpoint.best_model_path)
+    fx_dm.setup()
+    fx_infer = RaceModule.load_from_checkpoint("models/ckpts/fx-epoch=00-val_loss=5.6027322.ckpt")
     fx_infer.eval()
 
-    test_batch_fn = lambda batch: (batch['inputs']['input_ids'], batch['targets']['input_ids'])
+    test_batch_fn = lambda batch: (batch['answers']['input_ids'], torch.cat([batch['answers']['input_ids'],batch['articles']['input_ids']], dim=1), batch['questions']['input_ids'])
     display_result_as_string(fx_dm.tokenizer, fx_dm.test_dataloader(),
                              fx_infer, test_batch_fn)
