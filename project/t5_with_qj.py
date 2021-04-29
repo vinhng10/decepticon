ecbc957697d0cc8cca97af42ef98ac546a5a79f1
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-04-29 10:00:22 +0200

    Update t5_with_qj.py

--- project/t5_with_qj.py
+++ project/t5_with_qj.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     batch_fn = None
     collate_fn = t5_collate_fn
-    NUM_SAMPLES = 7
+    NUM_SAMPLES = 5
 
     pl.seed_everything(1234)
 
@@ -48,11 +48,11 @@ if __name__ == "__main__":
     fx_model.setup_tune(top_p = 0.95, top_k = 50, no_repeat_ngram_size = 2, num_samples = NUM_SAMPLES)
     
     
-    qj_model = BertForSequenceClassification.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator").cuda()
-    qj_tokenizer  = AutoTokenizer.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator")
+#     qj_model = BertForSequenceClassification.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator").cuda()
+#     qj_tokenizer  = AutoTokenizer.from_pretrained("iarfmoose/bert-base-cased-qa-evaluator")
     
     fx_model.eval()
-    qj_model.eval()
+#     qj_model.eval()
     
     metrics = Metrics()
     
@@ -81,22 +81,22 @@ if __name__ == "__main__":
             for target in y["input_ids"]
         ]
         
-        context = [ fx_model.tokenizer_.decode(c, skip_special_tokens=False\
-                                              ).replace("[ANS]", "").split("[CON]")[0] \
-                   for c in torch.repeat_interleave(x["input_ids"], NUM_SAMPLES, dim = 0)]
+#         context = [ fx_model.tokenizer_.decode(c, skip_special_tokens=False\
+#                                               ).replace("[ANS]", "").split("[CON]")[0] \
+#                    for c in torch.repeat_interleave(x["input_ids"], NUM_SAMPLES, dim = 0)]
         
-        x2 = zip(predictions, context)
-        inputs = []
+#         x2 = zip(predictions, context)
+#         inputs = []
         
-        for item in x2:
-            inputs.append(" ".join(["[CLS]", item[0], "[SEP]", item[1]]))
-        inputs = qj_tokenizer(inputs, padding=True, truncation=True, max_length=512, return_tensors="pt")
+#         for item in x2:
+#             inputs.append(" ".join(["[CLS]", item[0], "[SEP]", item[1]]))
+#         inputs = qj_tokenizer(inputs, padding=True, truncation=True, max_length=512, return_tensors="pt")
         
-        output2 = F.softmax(qj_model.forward(inputs["input_ids"].cuda())["logits"], dim=1).squeeze()[:,0]
-        good_args = torch.nonzero(output2 > 0.99).cpu().numpy().squeeze()
+#         output2 = F.softmax(qj_model.forward(inputs["input_ids"].cuda())["logits"], dim=1).squeeze()[:,0]
+#         good_args = torch.nonzero(output2 > 0.99).cpu().numpy().squeeze()
         
-        predictions = [predictions[i] for i in good_args]
-        references = [references[i] for i in good_args]
+#         predictions = [predictions[i] for i in good_args]
+#         references = [references[i] for i in good_args]
         
         # Compute metrics:
         inputs = Input(predictions=predictions, references=references)
