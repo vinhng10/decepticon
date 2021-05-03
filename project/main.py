f5d7a5ecf617c8565052f23318407264b44267ec
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-05-03 22:56:18 +0300

    update: add import and fix style

--- project/main.py
+++ project/main.py
@@ -18,8 +18,8 @@ from data.data import RaceDataModule
 from utils.utils import (
     serialize_config,
     t5_collate_fn, t5_dis_collate_fn,
-    transformer_collate_fn,default_collate_fn, transformer_test_batch_fn,
-    rnn_collate_fn, rnn_dis_collate_fn,  rnn_test_batch_fn,
+    transformer_collate_fn, default_collate_fn, transformer_test_batch_fn,
+    rnn_collate_fn, rnn_dis_collate_fn, rnn_test_batch_fn, rnn_dis_batch_fn,
     display_result_as_string,
 )
 
